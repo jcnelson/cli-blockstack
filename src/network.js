b23bb11b3122133f8a8ec33c16fe178c0b53a2c1
Jude Nelson <judecn@gmail.com>
2018-04-03 18:47:52 -0400

    add some helper methods to our modified Network object that can be folded into blockstack.js at some point

--- src/network.js
+++ src/network.js
@@ -58,6 +58,84 @@ export class CLIRegtestNetworkAdapter extends blockstack.network.LocalRegtest {
         }
       });
   }
+
+  putZonefile(zonefileData: string) : Promise<*> {
+    // TODO: consider moving this to blockstack.js
+    const requestHeaders = {
+      Accept: 'application/octet-stream',
+      'Content-Type': 'application/octet-stream'
+    };
+
+    const options = {
+      method: 'PUT',
+      headers: requestHeaders,
+      body: zonefileData
+    };
+
+    const url = `${this.blockstackAPIUrl}/v1/zonefiles`;
+    return fetch(url, options)
+      .then(resp => {
+        if (resp.status === 200 || resp.status === 202) {
+          return {'status': true};
+        }
+        else {
+          throw new Error(`Failed to store zonefile: status code ${resp.status}`);
+        }
+      });
+  }
+
+  getBlockchainNameRecord(name: string) : Promise<*> {
+    // TODO: consider moving this to blockstack.js
+    const url = `${this.blockstackAPIUrl}/v1/blockchains/bitcoin/${name}`;
+    return fetch(url)
+      .then(resp => resp.json())
+      .then((nameInfo) => {
+        // coerce all addresses
+        let fixedAddresses = {};
+        for (let addrAttr of ['address', 'importer_address', 'recipient_address']) {
+          if (nameInfo.hasOwnProperty(addrAttr) && nameInfo[addrAttr]) {
+            fixedAddresses[addrAttr] = this.coerceAddress(nameInfo[addrAttr]);
+          }
+        }
+        return Object.assign(nameInfo, fixedAddresses);
+    });
+  }
+
+  getNameHistory(name: string, 
+                 startHeight: number | null, 
+                 endHeight: number | null) : Promise<*> {
+
+    // TODO: consider moving this to blockstack.js
+    let url = `${this.blockstackAPIUrl}/v1/names/${name}/history`;
+    if (startHeight !== null) {
+      url += `?start_block=${startHeight}`;
+    }
+    if (endHeight !== null) {
+      url += `&end_block=${endHeight}`;
+    }
+    return fetch(url)
+      .then(resp => resp.json())
+      .then((historyInfo) => {
+        // coerce all addresses 
+        let fixedHistory = {};
+        for (let historyBlock of Object.keys(historyInfo)) {
+          let fixedHistoryList = []
+          for (let historyEntry of historyInfo[historyBlock]) {
+            let fixedAddresses = {};
+            let fixedHistoryEntry = null; 
+            for (let addrAttr of ['address', 'importer_address', 'recipient_address']) {
+              if (historyEntry.hasOwnProperty(addrAttr) && historyEntry[addrAttr]) {
+                fixedAddresses[addrAttr] = this.coerceAddress(historyEntry[addrAttr]);
+              }
+            }
+            fixedHistoryEntry = Object.assign(historyEntry, fixedAddresses);
+            fixedHistoryList.push(fixedHistoryEntry);
+          }
+          fixedHistory[historyBlock] = fixedHistoryList;
+        }
+        return fixedHistory;
+      });
+  }
 }
 
 /*
