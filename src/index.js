0475e5079e743b80f4de897978b53cdc81702071
Jude Nelson <judecn@gmail.com>
2018-07-01 23:21:48 -0400

    implement enough of window and localstorage in RAM so blockstack.js can handle mocked sign-ins

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
 
 export { CLIMain } from './cli';
+
+// implement just enough of window to be useful to blockstack.js.
+// do this here, so we can be *sure* it's in RAM.
+const localStorageRAM = {};
+
+global['window'] = {
+  location: {
+    origin: 'localhost'
+  },
+  localStorage: {
+    getItem: function(itemName) {
+      return localStorageRAM[itemName];
+    },
+    setItem: function(itemName, itemValue) {
+      localStorageRAM[itemName] = itemValue;
+    },
+    removeItem: function(itemName) {
+      delete localStorageRAM[itemName];
+    }
+  }
+};
+
+global['localStorage'] = global['window'].localStorage
+
 require('.').CLIMain()
